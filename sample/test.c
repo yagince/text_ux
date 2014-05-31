2443aa9dacf8fada8dea1589577439dc7fd078d6
yagince <straitwalk@gmail.com>
2014-05-31 22:28:32 +0900

    removed samples

--- sample/test.c
+++
@@ -1,8 +0,0 @@
-#include <ux/ux.hpp>
-#include <vector>
-
-vector keyList;
-keyList.push_back(string("レッドブル")) // 辞書登録したいキーワード集合
-keyList.push_back(string("ウォッカ")) // 辞書登録したいキーワード集合
-
-ux::Trie trie(keyList);
